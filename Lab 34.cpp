0cd7bbd91033eed0b9b1c2939aacaf785274c92e
Daniel-Aboop <danielsantisteban286@gmail.com>
2025-11-19 12:56:35 -0800

    Step 1 Complete

--- Lab 34.cpp
+++ Lab 34.cpp
@@ -2,9 +2,10 @@
 // IDE used: VS Code
 #include <iostream>
 #include <vector>
-
+#include <queue>
+#include <stack>
 using namespace std;
-const int SIZE=5;
+const int SIZE=7;
 struct Edge {
     int src, dest, weight;
 };
@@ -30,13 +31,63 @@ public:
     }
     void printGraph() {
         for (int i = 0; i < adjList.size(); i++) {
+            cout<<i<<"-->";
             for (Pair v: adjList[i])
-                cout << "(" << i << ", " << v.first << ", " << v.second << ") ";
+                cout << "("<< v.first << ", " << v.second << ") ";
             cout << endl;
         }
     }
+    void BFS(int start){
+        vector<bool> visited(SIZE, false);
+        queue<int> q;
+        visited[start] = true;
+        q.push(start);
+
+        cout << "BFS starting from node " << start << ": "<<endl;
+        while (!q.empty()) {
+            int node = q.front(); q.pop();
+            cout << node << " ";
+
+            for (auto &neighbor : adjList[node]) {
+                if (!visited[neighbor.first]) {
+                    visited[neighbor.first] = true;
+                    q.push(neighbor.first);
+                }
+            }
+        }
+        cout << endl;
+    }
+    void DFS(int start) {
+        vector<bool> visited(SIZE, false);
+        stack<int> s;
+        s.push(start);
+
+        cout << "DFS starting from node " << start << ": "<<endl;
+        while (!s.empty()) {
+            int node = s.top(); s.pop();
+            if (!visited[node]) {
+                visited[node] = true;
+                cout << node << " ";
+                for (auto &neighbor : adjList[node]) {
+                    if (!visited[neighbor.first])
+                        s.push(neighbor.first);
+                }
+            }
+        }
+        cout << endl;
+    }
 };
 int main() {
-    
+      vector<Edge> edges = {
+        // (x, y, w) —> edge from x to y having weight w
+        {0,1,12},{0,2,8},{0,3,21},{2,3,6},{2,6,2},{5,6,6},{4,5,9},{2,4,4},{2,5,5}
+    };
+    // Creates graph
+    Graph graph(edges);
+    graph.printGraph();
+    graph.DFS(0);
+    graph.BFS(0);
+
+
     return 0;
 }
